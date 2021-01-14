2d17397413bc9e4d529a68475538337b4a6856e0
khrapins <khrapins@gmail.com>
2021-01-14 16:57:04 +0200

    useRef() hook - to get access to DOM element

--- src/ReactHooks.js
+++ src/ReactHooks.js
@@ -1,10 +1,12 @@
 import MainContainer from './MainContainer'
 import ShowPreviousValue from './hooks/ShowPreviousValue'
+import FocusElement from './hooks/FocusElement'
 
 function ReactHooks() {
     return (
         <MainContainer>
             <ShowPreviousValue />
+            <FocusElement />
         </MainContainer>
     )
 }
---
+++ src/hooks/FocusElement.js
@@ -0,0 +1,23 @@
+import {useState, useRef} from 'react'
+import RowWrapper from '../utils/RowWrapper'
+import HookTitle from '../utils/HookTitle'
+
+function FocusElement() {
+    let [value, setValue] = useState('')
+    let input = useRef(null)
+
+    function handleButtonClick() {
+        input.current.focus()
+    }
+    
+    return (
+        <RowWrapper className="hook-useRef">
+            <HookTitle title="To get an access to DOM element" tag="h4"/>
+            <input ref={input} onChange={e => setValue(e.target .value)} value={value} />
+            <button onClick={handleButtonClick}>Focus</button>
+        </RowWrapper>
+    )
+}
+
+export default FocusElement
+
