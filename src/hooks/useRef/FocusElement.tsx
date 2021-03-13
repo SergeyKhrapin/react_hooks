5bdb33ba31362ffb877592e688d75152df42a366
SergeyKhrapin <khrapins@gmail.com>
2021-03-13 19:35:34 +0200

    useRef() hook - to handle input change

--- src/hooks/useRef/FocusElement.tsx
+++ src/hooks/useRef/FocusElement.tsx
@@ -13,7 +13,8 @@ const FocusElement: FC<HookName> = () => {
         <>
             <h4>To get an access to DOM element</h4>
             <div className='d-flex'>
-                <input ref={input} onChange={e => setValue(e.target .value)} value={value} />
+                {/* <input ref={input} onChange={() => setValue(input.current.value)} value={value} /> */}
+                <input ref={input} onChange={e => setValue(e.target.value)} value={value} />
                 <button className="btn btn-primary ms-2" onClick={handleButtonClick}>Focus</button>
             </div>
         </>
